70844e4fdeefbc7da5535cad8a768e0c41f37ac3
Artur Sargsyan <artursargsyan@Arturs-MacBook-Pro.local>
2019-12-12 10:00:12 -0500

    cool changes

--- Culminating.java
+++ Culminating.java
@@ -3,6 +3,9 @@
  * Shivam, Mikayel, Daniel
  * ICS 4U
  */
+
+ import java.util.Scanner;
+ 
 public class Culminating {
     public static void main(String[] args) {
         System.out.println("test");
