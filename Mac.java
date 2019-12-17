374020dc169a50585e0949f42c345843bbfdb7bf
Shivam Sharma <shivam.sharm@icloud.com>
2019-12-17 11:12:55 -0500

    Add Mac Variables

--- Mac.java
+++ Mac.java
@@ -8,36 +8,50 @@
  */
 public class Mac extends Product {
 
-    public Mac(String n, double p) {
+    private int numProcessorOptions;
+    private int numMemoryOptions;
+    private int numGraphicsOptions;
+    private String[] processors = new String[numProcessorOptions];
+    private String[] memory = new String[numMemoryOptions];
+    private String[] graphics = new String[numGraphicsOptions];
+
+    public Mac(String n, double p, int nPO, int nMO, int nGO) {
         super(n, p);
-        // TODO Auto-generated constructor stub
+        numProcessorOptions = nPO;
+        numMemoryOptions = nMO;
+        numGraphicsOptions = nGO;
     }
 
 }
 
 /*
-Mac Pro [$]
+Mac Pro [$7,499 +]
 Processor:
-    28-Core 2.5GHz Intel Xeon W
+    8-Core 3.5GHz Intel Xeon W
+    12-Core 3.3GHz Intel Xeon W     + $1,200
+    16-Core 3.2GHz Intel Xeon W     + $2,400
+    24-Core 2.7GHz Intel Xeon W     + $7,200
+    28-Core 2.5GHz Intel Xeon W     + $8,400
 Memory:
     32 GB
-    48 GB
-    96 GB
-    192 GB
-    384 GB
-    768 GB
-    1.5 TB
+    48 GB   + $360
+    96 GB   + $1,200
+    192 GB  + $3,600
+    384 GB  + $7,200
+    768 GB  + $16,800
+    768 GB  + $12,000
+    1.5 TB  + $30,000
 Graphics:
     AMD Radeon Pro 580X
-    AMD Radeon Pro W5700X
-    AMD Radeon Pro Vega II
-    AMD Radeon Pro Vega II Duo
+    AMD Radeon Pro Vega II      + $
+    Two AMD Radeon Pro Vega II  + $
+    AMD Radeon Pro Vega II Duo  + $
 Storage:
     256 GB
-    1 TB
-    2 TB
-    4 TB
-    8 TB
+    1 TB    + $
+    2 TB    + $
+    4 TB    + $
+    8 TB    + $
 Colours: 
     Silver
 Display:
